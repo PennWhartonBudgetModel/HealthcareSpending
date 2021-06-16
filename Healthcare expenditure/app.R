a541f76d559413957eec09c33a6ece5cb735231b
Yan_PWBM <yanhe8@wharton.upenn.edu>
2021-06-16 16:03:30 -0400

    finish the last part of the health facts APP

--- Healthcare expenditure/app.R
+++ Healthcare expenditure/app.R
@@ -51,9 +51,11 @@ server <- function(input, output) {
 
     df <- mcr %>% bind_rows(mcd) %>% bind_rows(phi) %>% bind_rows(unins) %>%
       filter(byvar %in% input$demolevels1)
-
+    
+    df["instype"] <- factor(df$instype, levels = c("Medicare", "Medicaid", "Private", 
+                                                   "No Insurance"))
+    
     # define the title based on different 
-
     ggplot(df) +
       # ggplot() +
       geom_line(aes(x = Year, y = value, group=byvar, color = byvar))+
@@ -126,6 +128,10 @@ server <- function(input, output) {
       temp['service'] <- service
       oop <- oop %>% bind_rows(temp)
     }
+    
+    oop["service"] <- factor(oop$service,levels = c("Total","Hospitalization", "Outpatient", 
+                                                    "ER", "Dental", "Homehealth", "Rx", 
+                                                    "MedEquip"))
     oop <- oop %>%
       filter(group %in% input$demolevels2)
     
@@ -141,9 +147,76 @@ server <- function(input, output) {
     choice <-  levels(meps[,input$demo2])
     checkboxGroupInput("demolevels2", paste0("Select ", input$demo2), 
                        choices = choice, selected = choice)
-    
   }) 
+
+  #-------------------------
+  # Third Graph
+  #-------------------------  
+  output$plot3 <- renderPlot({
     
+    service <- input$service3
+    
+    expvarlist <- c("TotalExp", "MedicareExp", "MedicaidExp", 
+                    "PHIandTricareExp", "OtherExp", "OOPExp")
+    varlist <- lapply(service, paste0, expvarlist)[[1]]
+    
+    df_prep <- function(exp, grp, pay) {
+      
+      df <- meps[meps$Year==input$year3 & meps[exp]>0,c('Year', "FinalWeight", exp, grp)]
+      df['group'] <- df[, grp]
+      df['expenditure'] <- df[,exp]
+      
+      df <- df %>%
+        group_by(Year, group) %>%
+        summarise(Population = sum(FinalWeight),
+                  Spending = sum(FinalWeight*expenditure)) %>%
+        group_by(Year) %>%
+        mutate(TotalPop = sum(Population),
+               TotalExp = sum(Spending),
+               PopShare = Population/TotalPop*100,
+               ExpShare = Spending/TotalExp*100) %>%
+        select(Year, group, PopShare, ExpShare) %>%
+        pivot_longer(cols=c("PopShare", "ExpShare"),  # reshape from wide to long
+                     names_to = 'var',
+                     values_to = 'value') 
+      df$var <- factor(df$var, levels =c("PopShare", "ExpShare"))
+      df['PaymentType'] <- pay
+      return(df)
+    }
+    
+    tot <- df_prep(varlist[1], input$demo3, "Total")
+    mcr <- df_prep(varlist[2], input$demo3, "Medicare")
+    mcd <- df_prep(varlist[3], input$demo3, "Medicaid")
+    phi <- df_prep(varlist[4], input$demo3, "PHI/Tricare")
+    oth <- df_prep(varlist[5], input$demo3, "Other insurance")
+    oop <- df_prep(varlist[6], input$demo3, "OOP")
+    
+    df <- tot %>% bind_rows(mcr) %>% bind_rows(mcd) %>% bind_rows(phi) %>%
+      bind_rows(oth) %>% bind_rows(oop)
+    
+    df["PaymentType"] <- factor(df$PaymentType, levels = c("Total", "Medicare", "Medicaid", "PHI/Tricare", "Other insurance", "OOP")) 
+    df <- df %>%
+      filter(group %in% input$demolevels3)
+    
+    # plot
+    df %>%
+      ggplot(aes(x = var, y = value, fill = group)) +
+      geom_bar(stat = "identity") +
+      scale_x_discrete(labels=c("PopShare" = "Population", 
+                                "ExpShare" = "Expenditure")) +
+      theme_bw()+
+      theme(axis.text.x = element_text(angle = 20)) +
+    geom_text(aes(label = paste0(round(value,1), '%')), size = 3, position = position_stack(vjust = 0.5)) +
+      facet_grid(~PaymentType) +
+      labs(title= "Share of population and corresponding spending", y="Share (%)", x = "") 
+    
+  })
+  output$checkbox3 <- renderUI({
+    choice <-  levels(meps[,input$demo3])
+    checkboxGroupInput("demolevels3", paste0("Select ", input$demo3), 
+                       choices = choice, selected = choice)
+  })
+      
 }
 
 ############### UI ####################
@@ -252,9 +325,55 @@ ui <- shinyUI(fluidPage(
                       plotOutput("plot2")
                       # htmlOutput('text')
                     ))
-    )# 2nd Graph Tab
-    
+    ), # 2nd Graph Tab
+    #-------------------------
+    # OOP Share Tab
+    #-------------------------  
+    tabPanel(
+      # Second tab
+      "Healthcare Expenditure Distribution",
+      headerPanel("Plotting healthcare expenditure distribution"),
+      # Side bar layout
+      sidebarLayout(position = "left",
+                    # Side panel
+                    sidebarPanel(width = 3,
+                                 selectInput("year3",
+                                             "Year",
+                                             c(seq(2008, 2018, 1)), 
+                                             selected = (2018)),
+                                 selectInput("service3",
+                                             "Health service:",
+                                             c('Total' = 'Total',
+                                               'Hospitalization' = 'Hospitalization', 
+                                               'Outpatient' = 'Outpatient', 
+                                               'ER' = 'ER',
+                                               'Dental' = 'Dental', 
+                                               'Home health' = 'Homehealth',
+                                               'Prescription drugs' = 'Rx',
+                                               'Vision & Medical equipment' = 'MedEquip'), 
+                                             selected = ('Total' = 'Total')),
+                                 selectInput("demo3",
+                                             "Demographics:",
+                                             c('Age Group' = 'AgeGroup', 
+                                               'Gender' = 'Gender', 
+                                               'Race' = 'Race',
+                                               'Marital Status' = 'MaritalStatus', 
+                                               'Education' = 'Education', 
+                                               'Employment Status' = 'EmploymentStatus',
+                                               'Poverty Status' = 'PovertyStatus',
+                                               'Insurance Type' = 'CoverageType',
+                                               'Region' = 'RegionMEPS'), 
+                                             selected = ('Age Group' = 'AgeGroup')),
+                                 uiOutput("checkbox3"),
+                                 helpText("Red dots are Elementary school, Blue dots are Middle school or above")
+                    ),
+                    # Main panel
+                    mainPanel(
+                      # Create the table.
+                      plotOutput("plot3")
+                      # htmlOutput('text')
+                    ))
+    )# 3rd Graph Tab
   )
 ))
 shinyApp(ui = ui, server = server)
-
